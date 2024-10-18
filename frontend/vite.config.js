ecb230f3b96c438174ea1c881492bcf4bb3ccfa6
sandhiyaR99 <sandhiyaravikumar365@gmail.com>
2024-10-18 23:38:02 +0530

    added vercel json 5

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Specify the output directory
+  },
+  // Optional: Base path for deployment, can be adjusted based on your app's routing
+  base: '/', // Use '/' for the root of the domain
+});
